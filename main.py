c3f91a7a32ccf9eff28d909b328a8dc566cb7941
Taylor Randall <taylor.k.randall@gmail.com>
2020-04-20 13:20:57 -0700

    ready for OSC

--- main.py
+++ main.py
@@ -5,20 +5,31 @@ from kivy.clock import Clock
 from kivy.uix.widget import Widget
 from kivy.config import Config
 from kivy.properties import (
-    NumericProperty, ReferenceListProperty, ObjectProperty)
+    NumericProperty, ObjectProperty)
+from kivy.event import EventDispatcher
 import time
 
 # Config.set('graphics','fullscreen','auto')
 print('before TimerApp')
+# seconds=5
 
-seconds=10
+class OSC_Dispatcher(EventDispatcher):
+    def __init__(self,**kwargs):
+        self.register_event_type('on_test')
+        super(OSC_Dispatcher,self).__init__(**kwargs)
+
+    def do_someting(self,value):
+        self.dispatch('on_test',value)
+
+    def on_test(self,*args):
+        print('i am dispatches',args)
 
 class Timer(Widget):
-    # seconds=NumericProperty(20)
-    timer=ObjectProperty('00:00:00')
+    seconds=NumericProperty(5)
+    timer=ObjectProperty(None)
 
     def updateEndTime(self):
-        endTime=time.time()+seconds
+        endTime=time.time()+self.seconds
         return endTime
 
     def startTimer(self):
@@ -26,13 +37,17 @@ class Timer(Widget):
 
     def runTimer(self,endTime):
         print('starting timer...')
-        for i in range(seconds):
+        for i in range(self.seconds):
             tmr=time.gmtime(endTime-time.time())
             print(time.strftime('%H:%M:%S', tmr))
-            time.sleep(1)
     
+    print('Timer defined...')
+
+print('???')
+
 class TimerApp(App):
     def build(self):
+        Timer().startTimer()
         return Timer()
 
 print('beofore .run()')
@@ -41,3 +56,10 @@ if __name__ == '__main__':
     TimerApp().run()
 
 print('end...')
+
+'''
+I need to update the Label text with a Clock object every second until the timer 
+reaches zero, then ClockEvent.cancle() the schedule_interval()
+
+
+'''
